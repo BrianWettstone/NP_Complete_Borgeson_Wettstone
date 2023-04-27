930a413d27a9fc6f772178b406d9ee9db97e0f29
Brian Wettstone <BrianWettstone@MacBook-Pro.local>
2023-04-27 12:08:19 -0400

    17 test cases

--- exact_solution/test_cases/run_test_cases.sh
+++ exact_solution/test_cases/run_test_cases.sh
@@ -11,5 +11,37 @@ echo "test 5 - should be unsatisfiable"
 python3 ../exact_solution.py < test5.txt
 echo "test 6 - should be unsatisfiable"
 python3 ../exact_solution.py < test6.txt
-#echo "test 7 - should be unsatisfiable"
-#python3 ../exact_solution.py < test7.txt
+echo $(date)
+echo "test 7 - should be unsatisfiable"
+python3 ../exact_solution.py < test7.txt
+echo $(date)
+echo "test 8 - should be unsatisfiable"
+python3 ../exact_solution.py < test8.txt
+echo $(date)
+echo "test 9 - should be unsatisfiable"
+python3 ../exact_solution.py < test9.txt
+echo $(date)
+echo "test 10 - should be unsatisfiable"
+python3 ../exact_solution.py < test10.txt
+echo $(date)
+echo "test 11 - should be unsatisfiable"
+python3 ../exact_solution.py < test11.txt
+echo $(date)
+echo "test 12 - should be unsatisfiable"
+python3 ../exact_solution.py < test12.txt
+echo $(date)
+echo "test 13 - should be unsatisfiable"
+python3 ../exact_solution.py < test13.txt
+echo $(date)
+echo "test 14 - should be unsatisfiable"
+python3 ../exact_solution.py < test14.txt
+echo $(date)
+echo "test 15 - should be unsatisfiable"
+python3 ../exact_solution.py < test15.txt
+echo $(date)
+echo "test 16 - should be unsatisfiable"
+python3 ../exact_solution.py < test16.txt
+echo $(date)
+echo "test 17 - should be unsatisfiable"
+python3 ../exact_solution.py < test17.txt
+echo $(date)
